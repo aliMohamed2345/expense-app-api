605e1ff2c7da40aaedd64516e40bb2008df50438
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-06-07 11:26:04 +0300

    starting the project

---
+++ api/server.js
@@ -0,0 +1,19 @@
+import env from 'dotenv';
+import express from 'express';
+
+env.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>")
+})
+
+
+
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
+
+
+
