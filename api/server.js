98cfbb20e13ec486156981a31fda4e8bcc5d1eae
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-06-07 12:57:05 +0300

    add the main folders

--- api/server.js
+++ api/server.js
@@ -9,13 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()) // parsing the json data
 app.use(express.urlencoded({ extended: true })) // parsing the urlencoded data
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
-})
+app.get("/", (req, res) => {
+    try {
+        res.status(200).send({ success: true, message: `hello world` });
+    } catch (error) {
+        res.status(500).send({ success: false, message: `internal server error` });
+    }
+});
 
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
 
-module.exports = app; // Export the app for testing purposes , This allows us to import the app in our test files without starting the server
-
-
+export default app; // Export the app for testing purposes , This allows us to import the app in our test files without starting the server
