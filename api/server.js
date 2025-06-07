791f87bcfcf194b9d7e744bd306ebf0e5c679fd0
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-06-07 12:39:40 +0300

    edit

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
+// app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
 
 module.exports = app; // Export the app for testing purposes , This allows us to import the app in our test files without starting the server
 
