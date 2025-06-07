48e70747ba556e3d39c9ba77d6562f7acd2e49fb
aliMohamed2345 <eloymohamedalimansor@gmail.com>
2025-06-07 12:11:51 +0300

    Export the app for testing purposes

--- api/server.js
+++ api/server.js
@@ -6,14 +6,16 @@ env.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()) // parsing the json data
+app.use(express.urlencoded({ extended: true })) // parsing the urlencoded data
+
 app.get('/', (req, res) => {
     res.send("<h1>Hello World</h1>")
 })
 
 
-
-
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
 
+module.exports = app; // Export the app for testing purposes , This allows us to import the app in our test files without starting the server
 
 
